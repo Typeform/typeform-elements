82cee46245ea593cef341afc5beec444cc1ded0b
Nicolas Grenié <nicolas.grenie@gmail.com>
2020-04-02 11:48:07 -0700

    chore(release): patch version release

--- src/shared/validate-url/index.ts
+++ src/shared/validate-url/index.ts
@@ -1,4 +1,4 @@
 import { html, TemplateResult } from 'lit-element';
 
 export const invalidUrlTemplate = (): TemplateResult => html`<p>Invalid Typeform URL</p>`;
-export const validateUrl = (url: string): boolean => /^(http|https):\/\/.+.typeform.com\/to\/(\w){6}($|\?.*$)/.test(url);
+export const validateUrl = (url: string): boolean => /^(https?):\/\/.+\.typeform\.com\/to\/([0-9a-zA-Z]{6,8})([$?].+)?/.test(url);
