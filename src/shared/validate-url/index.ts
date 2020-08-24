6e263e400539b13b88b4ec93d5313ea5ce406705
Nicolas Grenié <nicolas.grenie@gmail.com>
2020-08-24 12:02:39 -0700

    add support to new typeform ids

--- src/shared/validate-url/index.ts
+++ src/shared/validate-url/index.ts
@@ -1,4 +1,4 @@
 import { html, TemplateResult } from 'lit-element';
 
-export const invalidUrlTemplate = (): TemplateResult => html`<p>Invalid Typeform URL</p>`;
+export const invalidUrlTemplate = (): TemplateResult => html`<p style="color: darkred;">Invalid Typeform URL <br/><small><a href="https://github.com/Typeform/typeform-elements">Typeform Elements</a></small></p>`;
 export const validateUrl = (url: string): boolean => /^(https?):\/\/.+\.typeform\.com\/to\/([0-9a-zA-Z]{6,8})([$?].+)?/.test(url);
